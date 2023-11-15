de608e43172f88014fd3b4cfcaafc2df15de00c7
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2023-11-15 16:41:04 +0100

    creation

--- CreationSQL.sql
+++ CreationSQL.sql
@@ -1,30 +1,30 @@
-drop table MCommunication;
-drop table MMessage;
-drop table MContact;
-drop table MPersonne;
+drop table if exists MCommunication;
+drop table if exists MMessage;
+drop table if exists MContact;
+drop table if exists MUtilisateur;
 
-create table MCommunication(
-idPersonneE REFERENCE(),
-idPersonneR REFERENCE(),
-idMessage REFERENCE()
+create table MUtilisateur(
+	idUtilisateur	int PRIMARY KEY,
+	prenom			varchar(20),
+	dateConnexion	date
 );
 
 create table MMessage(
-idMessage int PRIMARY KEY,
-personne REFERENCE(),
-dateEnvois date;
+	idMessage	int	PRIMARY KEY,
+	dateEnvois	date
 );
 
 create table MContact(
-idContact int PRIMARY KEY,
-idPersonneContact int REFERENCE(),
-idPersonneCreation int REFERENCE(),
-pseudo varchar(20),
-dateCreation date
+	idContact				int PRIMARY KEY,
+	idUtilisateurContact	int REFERENCES MUtilisateur(idUtilisateur),
+	idUtilisateurCreation	int REFERENCES MUtilisateur(idUtilisateur),
+	pseudo					varchar(20),
+	dateCreation			date
 );
 
-create table MPersonne(
-idPersonne int PRIMARY KEY,
-prenom varchar(20),
-dateConnexion date
+create table MCommunication(
+	idUserSource		int REFERENCES MUtilisateur(idUtilisateur),
+	idUserDestination	int REFERENCES MUtilisateur(idUtilisateur),
+	idMessage			int REFERENCES MMEssage(idMessage),
+	PRIMARY KEY ( idUserSource, idUserDestination, idMessage )
 );
